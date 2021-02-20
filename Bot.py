9ce0b3a2da38b0f3714461e1650e1fbb682ed386
aakzsh <69726390+aakzsh@users.noreply.github.com>
2021-02-20 22:11:48 +0530

    add

--- Bot.py
+++ Bot.py
@@ -3,6 +3,7 @@ import os
 import requests
 import json
 import random
+from datetime import datetime
 
 client = discord.Client()
 sad_words = ["sad", "depressed", "unhappy", "angry", "miserable"]
@@ -25,17 +26,21 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    
 
   if message.author == client.user:
     return
 
   msg = message.content
-
+  now = datetime.now()
+  current_time1 = now.strftime("%H:%M:%S")
+  current_time2 = now.strftime("%H:%M:%S")
+  
   if any(word in msg for word in sad_words):
     await message.channel.send(random.choice(starter_encouragements))
   
   if message.content.startswith('seekhealth'):
-      await message.channel.send("Here is a list of commands you can use for this bot\n\n!bmi - to calculate you body mass index and tell if its in the acceptable range or not\n\n!diet - to tell you how much should be your daily calorie intake according to your needs\n\n!meditate - to let you meditate and calculate the duration for which you meditated\n  !startmeditation - start meditation\n  !stopmeditation - stop meditation and see the duration for which you meditated\n\n!playmusic - play a playlist to according to your choice for either meditation, yoga or workout")
+      await message.channel.send("Here is a list of commands you can use for this bot\n\n!bmi - to calculate you body mass index and tell if its in the acceptable range or not\n\n!diet - to tell you how much should be your daily calorie intake according to your needs\n\n!meditate - to let you meditate and calculate the duration for which you meditated\n  !startmeditation - start meditation\n  !stopmeditation - stop meditation and see the duration for which you meditated\n\n!playmusic - play a playlist to according to your choice for either meditation, yoga or workout\n  !meditationmusic - play music playlist best suitable for meditation\n  !workoutmusic - play music playlist best suitable for workout\n  !yogamusic - play music playlist best suitable for yoga")
 
   elif message.content.startswith('!inspire'):
     quote = get_quote()
@@ -55,7 +60,18 @@ async def on_message(message):
 
   elif message.content.startswith('!meditate'):
       {
-        await message.channel.send("use command '!startmeditation' and start meditating. When you're done, use command '!stopmeditation' to see how long did you meditate :)")
+          await message.channel.send("use command '!startmeditation' and start meditating. When you're done, use command '!stopmeditation' to see how long did you meditate :)")
+      }
+  elif message.content.startswith('!startmeditation'):
+      {
+        
+        await message.channel.send("current time:" + current_time1 + " \ngoodluck with your meditation")
+      }
+
+  elif message.content.startswith('!stopmeditation'):
+      {
+        
+        await message.channel.send("current time:" + current_time2 + " \nyou meditated for a total of " + "*gotta include time difference here*")
       }
   
   elif message.content.startswith('!playmusic'):
